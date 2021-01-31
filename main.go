c75b1608e0b177be2962e78eb85de061d1275495
pcrandall <crandall.phillip@gmail.com>
2021-01-31 13:20:56 -0600

    added arp package for windows arp lookup

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"github.com/go-ping/ping"
+	"github.com/pcrandall/networkscanner/arp"
 	"github.com/pcrandall/networkscanner/network"
 )
 
@@ -32,84 +33,80 @@ type online struct {
 var (
 	wg        sync.WaitGroup
 	available []string
+	debug     bool
+	write     bool
+	silent    bool
+	err       error
 )
 
 func main() {
-
-	ip := flag.String("ip", "192.168.1.1/24", "-ip=192.168.1.1/24 CIDRblock to scan.\nOnly CIDR format supported at this time.")
-	t := flag.Int("t", 500, "-t=500 timeout in milliseconds")
-	c := flag.Int("c", 1, "-c=1 number of times to ping")
-	silent := flag.Bool("s", false, "-s=true no output to console")
-	write := flag.Bool("w", false, "-w=true write output to ips.txt")
-
+	// flags
+	ip := flag.String("ip", "192.168.1.1/24", "default:-ip=192.168.1.1/24 CIDRblock to scan.\nOnly CIDR format supported at this time.")
+	t := flag.Int("t", 1000, "default:-t=1000 Timeout in milliseconds)")
+	c := flag.Int("c", 2, "default:-c=2 Number of times to ping")
+	i := flag.Int("i", 250, "default:-i=250 Ping interval")
+	// interval := time.Duration(*flag.Duration("i", 250, "default:-i=250 Ping interval")) * time.Millisecond
+	debug = *flag.Bool("d", false, "default:false Debug")
+	silent = *flag.Bool("s", false, "default:-s=false No output to console")
+	write = *flag.Bool("w", false, "default:-w=false Write output to ips.txt in current directory")
 	flag.Parse()
 
-	count := c
-	timeout := time.Duration(*t) * time.Millisecond
-	fmt.Println(count)
+	output, err := os.Create("ips.txt")
+	if err != nil {
+		panic(err)
+	}
 
+	count := *c
+	timeout := time.Duration(*t) * time.Millisecond
+	interval := time.Duration(*i) * time.Millisecond
 	peerConn := make(map[string]*Peer)
 	unavailable := make(map[string]*online)
 
 	_, IPrange := network.CalculateCIDR(*ip)
 
+	// arp lookup
+	for ip, _ := range arp.Table() {
+		go func(ip string) {
+			mac := arp.Search(ip)
+
+			if mac != "" {
+				removeAvailable(ip)
+			}
+
+			fmt.Println(ip, " mac: ", mac)
+		}(ip)
+	}
+
 	for _, ip := range IPrange {
-
+		available = append(available, ip) // create slice to remove all unavailable addresses later
 		wg.Add(1)
-
 		ctx := context.Background()
-
 		peerConn[ip] = &Peer{
 			Address:  ip,
 			Ctx:      &ctx,
-			Count:    4,
+			Count:    count,
 			PingTime: timeout,
+			Interval: interval,
 		}
-
 		unavailable[ip] = &online{}
-
 		go Ping(ip, peerConn[ip].Ctx, peerConn[ip], unavailable[ip])
 	}
 
 	wg.Wait()
 
-	var output *os.File
-	var err error
-
-	if *write {
-		// Check if file exists
-		if _, err = os.Stat("ips.txt"); os.IsNotExist(err) {
-			output, err = os.Create("ips.txt")
-		} else { // If file exists, open it
-			os.Remove("ips.txt") // remove file and create new one
-			output, err = os.Create("ips.txt")
-			output, err = os.Open("ips.txt")
-		}
-	}
-
 	for _, val := range unavailable {
-		if val.bytes > 0 && !*silent && *write { // console output and write to file
-			fmt.Println(val.addr, "\tbytes rec:", val.bytes, "\ttime:", val.time)
-			// io.WriteString(output, val.addr+"\n")
-		} else if val.bytes > 0 && *silent && *write { // write to file
-			// io.WriteString(output, val.addr+"\n")
-		} else if val.bytes > 0 && !*silent && !*write { // conole output
-			fmt.Println(val.addr, "\tbytes rec:", val.bytes, "\ttime:", val.time)
+		if val.bytes > 0 && !silent { // console output and write to file
+			fmt.Println(val.addr, "is taken\tbytes rec:", val.bytes, "\ttime:", val.time)
 		}
 	}
 
-	if *write { // console output and write to file
-		for _, val := range available {
-			io.WriteString(output, val)
-		}
+	for _, val := range available {
+		io.WriteString(output, val+"\n")
 	}
-
-	fmt.Println(available)
 
 }
 
 func Ping(_ip string, ctx *context.Context, peer *Peer, unavailable *online) {
-
 	defer wg.Done()
 
 	pinger, err := ping.NewPinger(_ip)
@@ -129,34 +126,40 @@ func Ping(_ip string, ctx *context.Context, peer *Peer, unavailable *online) {
 		unavailable.addr = _ip
 		unavailable.bytes = pkt.Nbytes
 		unavailable.time = pkt.Rtt
-		// fmt.Printf("%d bytes from %s: icmp_seq=%d time=%v ttl=%v\n",
-		// 	pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt, pkt.Ttl)
+		if debug {
+			fmt.Printf("%d bytes from %s: icmp_seq=%d time=%v ttl=%v\n",
+				pkt.Nbytes, pkt.IPAddr, pkt.Seq, pkt.Rtt, pkt.Ttl)
+		}
+		removeAvailable(_ip)
 	}
 
 	pinger.OnFinish = func(stats *ping.Statistics) {
-		// if stats.PacketLoss < 100 {
-		// 	fmt.Printf("\n--- %s ping statistics ---\n", stats.Addr)
-		// 	fmt.Printf("%d packets transmitted, %d packets received, %v%% packet loss\n",
-		// 		stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss)
-		// }
-
-		if stats.PacketLoss > 99.9 {
-			// fmt.Printf("\n%s Available ---%d packets transmitted, %d packets received, %v%% packet loss\n", stats.Addr, stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss)
-			s := fmt.Sprintf("%0f", stats.PacketLoss)
-			r := fmt.Sprintf("%x", stats.PacketsSent)
-			t := fmt.Sprintf("%x", stats.PacketsRecv)
-			available = append(available, string(stats.Addr)+" Available --- packets transmitted: "+r+" packets received: "+t+" packet loss "+s+"\n")
+		if debug {
+			fmt.Printf("\n--- %s ping statistics ---\n", stats.Addr)
 			fmt.Printf("%d packets transmitted, %d packets received, %v%% packet loss\n",
 				stats.PacketsSent, stats.PacketsRecv, stats.PacketLoss)
+			fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
+				stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)
 		}
-		// fmt.Printf("round-trip min/avg/max/stddev = %v/%v/%v/%v\n",
-		// 	stats.MinRtt, stats.AvgRtt, stats.MaxRtt, stats.StdDevRtt)
 		return
 	}
 
 	err = pinger.Run() // Blocks until finished.
-
 	if err != nil {
 		fmt.Printf("Failed to ping target host: %s", err)
+	}
+}
+
+func removeAvailable(ip string) {
+	if debug {
+		fmt.Println("in remove available", available)
+	}
+	for idx, val := range available {
+		if val == ip {
+			tmp := available[:idx]
+			available = available[idx+1 : len(available)-1]
+			tmp = append(tmp, available...)
+			available = tmp
+		}
 	}
 }
